a0fea9e30676c2a87775d7dd64ff29485325357d
Marcella <129318421+marcella28@users.noreply.github.com>
2023-11-10 17:39:10 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./screen/**/*.{html,js,css}"],
+  theme: {
+    colors: {
+      'lblue': '#C9E3E7',
+    extend: {},
+  },
+  plugins: [],
+}
+}
+
